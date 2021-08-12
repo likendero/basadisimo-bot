be0cb541649a2464c3fe32862a45f061255a3507
likendero <33293802+likendero@users.noreply.github.com>
2021-08-12 21:21:00 +0200

    se añaden mejoras como añadir frases desde un fichero o un menu

--- src/accionesBot.py
+++ src/accionesBot.py
@@ -1,12 +1,30 @@
 
 
+from typing import List
+import random
 from telegram.ext import Updater
 from telegram.ext import CallbackContext
 
-def basado(update: Updater ,context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="soy un bot basadisimo")
+frases = List()
+frases.append("no siento las piernas")
 
+# metodo para devolver una frase basada aleatoria
+def basado(update: Updater ,context: CallbackContext):
+    frase = random.choice(frases)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=frase)
+
+# metodo pos por probar el bot
 def start(update: Updater ,context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="empecemos la marcha")
+
+# metodo para recarga de las frases usables
+def recarga_frases(fich_frases="./frases.txt"):
+    try:
+        fichero = open(fich_frases,"r")
+        for linea in fichero:
+            frases.append(linea)
+        fichero.close()
+    except OSError as err:
+        print("oh ha ocurrido un error {0}".format(err))
 
 COMAND_DIC = {"start":start, "basado":basado}
--- src/basado-bot.py
+++ src/basado-bot.py
@@ -11,17 +11,38 @@ def add_handlers(dispacher:Dispatcher):
         command = CommandHandler(key,value)
         dispacher.add_handler(command)
 
+# fucnion para lanzar el mnu
+def menu():
+    stop = False
+    while not stop:
+        print("### MENU ###")
+        print(" - unput recarga to recargar frases")
+        print(" - input stop to stop: ")
+        input_value = input("que quieres hacer: ")
+        if input_value == "stop":
+            stop = True
+        elif input_value == input_value:
+            accionesBot.recarga_frases()
+
 # inicio Pavo!!!!
 def main():
     updater = Updater(constantesBot.TOKEN,use_context=constantesBot.CONTEXT)
     dispacher = updater.dispatcher
+
+    accionesBot.recarga_frases()
+
+    # se añaden los listeners
     add_handlers(dispacher)
+
+    # se empiezan a aceptar solicitudes
     updater.start_polling()
-    stop = False
-    while not stop:
-        input_value = input("input stop to stop: ")
-        if input_value == "stop":
-            stop = True
+    
+    # aqui """"paramos"""" el programa para poder indicar stop
+    # tambien es un menu para otras cosillas pero lo importante
+    # es poder parar el programa xD
+    menu()
+
+    #se dejan de aceptar solicitudes
     updater.stop()
 
 if __name__ == "__main__":
