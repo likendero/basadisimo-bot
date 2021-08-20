d305ba12e4cf07d16fba3c04379a5637c4fe7389
Juanjo Romero Cruz <juanolas73@gmail.com>
2021-08-20 22:54:17 +0200

    Hemos mejorado la vaina

--- src/accionesBot.py
+++ src/accionesBot.py
@@ -1,24 +1,24 @@
-
-
 from typing import List
 import random
 from telegram.ext import Updater
 from telegram.ext import CallbackContext
 
-frases = List()
+frases = List('')
 frases.append("no siento las piernas")
 
 # metodo para devolver una frase basada aleatoria
 def basado(update: Updater ,context: CallbackContext):
     frase = random.choice(frases)
+    
     context.bot.send_message(chat_id=update.effective_chat.id, text=frase)
 
 # metodo pos por probar el bot
 def start(update: Updater ,context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="empecemos la marcha")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="empecemos la marcha, señoritas.")
 
 # metodo para recarga de las frases usables
 def recarga_frases(fich_frases="./frases.txt"):
+    frases = []
     try:
         fichero = open(fich_frases,"r")
         for linea in fichero:
